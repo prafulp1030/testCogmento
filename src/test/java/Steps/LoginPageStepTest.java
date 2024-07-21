56a8669f35ea71c00aaf68f4e6f730202158d3e0
prafpawar11 <prafulp1030@gmail.com>
2024-07-21 18:05:13 +0530

    BDD framework is created

---
+++ src/test/java/Steps/LoginPageStepTest.java
@@ -0,0 +1,31 @@
+package Steps;
+
+import org.openqa.selenium.By;
+
+import BaseLayer.BaseClass;
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
+
+public class LoginPageStepTest extends BaseClass {
+
+	@Given("user is on login page")
+	public void user_is_on_login_page() {
+		BaseClass.initialization();
+	}
+
+	@When("user enter valid credentails")
+	public void user_enter_valid_credentails() {
+		driver.findElement(By.name("email")).sendKeys("prafulp1010@gmail.com");
+		driver.findElement(By.name("password")).sendKeys("Pr@ful0812");
+
+	}
+
+	@Then("user click on login button")
+	public void user_click_on_login_button() {
+
+		driver.findElement(By.xpath("//div[text()='Login']")).click();
+
+	}
+
+}
---
+++ target/test-classes/Features/cogmento.feature
@@ -0,0 +1,6 @@
+Feature: Test Cogmento CRM Application
+
+  Scenario: Validate Login Page Functionality
+    Given user is on login page
+    When user enter valid credentails
+    Then user click on login button
